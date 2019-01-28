33dc05db26a75b4fa50ef1b19f0bc269cf8ba88f
akshay-4079 <46236877+akshay-4079@users.noreply.github.com>
2019-01-28 11:55:12 +0530

    With alerts

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 var todo = angular.module('todoApp', ['ngStorage']);
 
 todo.controller('todoController', ['$scope', '$localStorage', function($scope, $localStorage) {
@@ -13,7 +15,8 @@ todo.controller('todoController', ['$scope', '$localStorage', function($scope, $
   };
   
   $scope.submitTask = function() {
-   var d= new Date($scope.mydate);
+   var d= $scope.mydate;
+      var e =$scope.mydate;
     var c= dateFormat(d,'mediumDate');
     $scope.tasks.push({
       taskName: $scope.name,
@@ -30,19 +33,36 @@ todo.controller('todoController', ['$scope', '$localStorage', function($scope, $
   };
   
   window.onload=function alerts(){
-    
+      var countb=0;
+          var countt=0;
+          var countf=0;
     for(var i=0;i<$scope.tasks.length;i++)
       {
         var dateTod=new Date();
        var t=$scope.tasks[i].due2;
-      var  c=dateTod-t;
-          
-            console.log(c);
+          console.log(t);
+     var a=moment(dateTod);
+          var b=moment(t);
+    var c=b.diff(a,'days');
+
+          if(c<0)   
+    {
+        
+    countb++;
           
       }
-    
+    if (c==0)
+  {
+      countt++;
   }
-  
-  
+  if(c>0)
+      {
+          countf++;
+      }
+      }
+  alert("You have "+ countb +" Tasks to be done before today." + countt + " tasks to be done by today and " + countf +" tasks to be done in the future");
+
+  };
+
 }]); 
 
